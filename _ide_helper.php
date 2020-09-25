02219ec94cf809e3eaf0705af6fe71547252bce3
wilfoz <github.com/wilfoz>
2020-09-25 15:32:37 -0300

    Resources refactored Category and add  Genres

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.0.1 on 2020-09-25 13:31:23.
+ * Generated for Laravel 6.0.1 on 2020-09-25 18:03:57.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
