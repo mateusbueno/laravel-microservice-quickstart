1e005bfe3750d87ab2ea8dbe0f82ce2f122e9554
wilfoz <github.com/wilfoz>
2020-10-07 21:58:16 -0300

    new files cloudbuild and docker-compose.prod

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -16,7 +16,7 @@ namespace App\Models{
  *
  * @property string $id
  * @property string $name
- * @property int $is_active
+ * @property bool $is_active
  * @property \Illuminate\Support\Carbon|null $deleted_at
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
@@ -45,7 +45,7 @@ namespace App\Models{
  * @property string $id
  * @property string $name
  * @property string|null $description
- * @property int $is_active
+ * @property bool $is_active
  * @property \Illuminate\Support\Carbon|null $deleted_at
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -12,7 +12,8 @@ class Genre extends Model
     protected $fillable = ['name', 'is_active'];
     protected $date = ['deleted_at'];
     protected $casts = [
-        'id' => 'string'
+        'id' => 'string',
+        'is_active' => 'boolean'
     ];
     public $incrementing = false;
 }
