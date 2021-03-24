d5cb34b01b12a0c3c0c406afe2b6d1294597f28e
wilfoz <wil_foz@yahoo.com.br>
2021-03-23 22:04:38 -0300

    chore: update docker/dbdata

--- backend/_ide_helper.php
+++ backend/_ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.20.14 on 2021-03-12 22:37:49.
+ * Generated for Laravel 6.20.14 on 2021-03-23 23:21:40.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
